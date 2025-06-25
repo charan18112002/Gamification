784f6349b5c7285c4ee129dced1efeecff7bfb9e
charan18112002 <charankarthik2002@gmail.com>
2025-06-25 12:36:22 +0100

    change1

---
+++ script.js
@@ -0,0 +1,9 @@
+function goToStudentGame() {
+  alert('Loading...');
+  // Example: window.location.href = 'student-game.html';
+}
+
+function goToQuiz() {
+  alert('Loading to Quiz...');
+  // Example: window.location.href = 'quiz.html';
+}
