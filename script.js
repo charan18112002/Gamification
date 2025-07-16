8e4e86469bef206018e9cc0a53c519cb66255f59
charan18112002 <charankarthik2002@gmail.com>
2025-07-16 17:22:08 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function goToStudentGames() {
-  alert('Loading...');
-  window.location.href = 'game.html';
-}
-
-function goToQuiz() {
-  alert('Loading...');
-  window.location.href = 'quiz.html';
-}
